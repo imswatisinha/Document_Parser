f8630c1e15865b958f94cdbdfda02414ff8f33d0
aryanboela <Aryanboela3@gmail.com>
2025-12-15 12:31:16 +0530

    Update audio_transcribe.py
    removing the showings of unecessary warnings

--- utils/audio_transcribe.py
+++ utils/audio_transcribe.py
@@ -3,32 +3,45 @@ import soundfile as sf
 import io
 from transformers import pipeline
 from scipy.signal import resample
+import warnings
+
+import logging
+# Suppress transformers and related logging (setting to ERROR or CRITICAL)
+logging.getLogger("transformers").setLevel(logging.ERROR)
 
 def audio_transcribe(audio_file: io.BytesIO) -> str:
-    try:    
+
+    def helper():
         # Use soundfile to read the buffer and get an array/sample rate
-        audio_data, sampling_rate = sf.read(audio_file)
-        if len(audio_data.shape) > 1:
-            audio_data = audio_data[:, 0]
+            audio_data, sampling_rate = sf.read(audio_file)
+            if len(audio_data.shape) > 1:
+                audio_data = audio_data[:, 0]
 
-        # resampling audio file for Whisper model
-        if sampling_rate != 16000:
-            audio_data = resample(audio_data, int(len(audio_data) * 16000 / sampling_rate))
-            sampling_rate = 16000
+            # resampling audio file for Whisper model
+            if sampling_rate != 16000:
+                audio_data = resample(audio_data, int(len(audio_data) * 16000 / sampling_rate))
+                sampling_rate = 16000
             
-        # whisper model setup
-        audio_pipeline = pipeline(
-            task = "automatic-speech-recognition",
-            model = "openai/whisper-small",
-            device = 0,
-            chunk_length_s = 60,
-            batch_size = 100
-        )
+            # whisper model setup
+            audio_pipeline = pipeline(
+                task = "automatic-speech-recognition",
+                model = "openai/whisper-small",
+                device = 0,
+                chunk_length_s = 60,
+                batch_size = 100
+            )
 
-        res = audio_pipeline(inputs = {"array": audio_data, "sampling_rate" : sampling_rate})
+            res = audio_pipeline(inputs = {"array": audio_data, "sampling_rate" : sampling_rate})
 
-        # returning the transcription
-        return res['text']
+            # returning the transcription
+            return res['text']
+
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            out = helper()
+
+        return out
+            
     except Exception as e:
-
         return None
